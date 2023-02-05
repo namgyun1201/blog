177e34e0084bb266ab294c7db39ed54d8ce23b14
namgyun1201@gmail.com <namgyun1201@gmail.com>
2023-02-05 20:09:25 +0900

    laracasts 12강까지 수강

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,26 +6,50 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post extends Model
 {
     use HasFactory;
 
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function fileList()
     {
         $files = File::files(resource_path("posts/"));
-        return array_map(fn ($file) => $file->getContents(), $files);
+        $posts = collect($files)
+        ->map(
+            function ($file) {
+                $document = YamlFrontMatter::parseFile($file);
+
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                );
+            }
+        );
+        return $posts;
     }
 
     public static function find($slug)
     {
-        base_path();
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-            // return redirect('/');
-            // abort(404);
-        }
-
-        return cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path));
+        return static::fileList()->firstWhere('slug', $slug);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
